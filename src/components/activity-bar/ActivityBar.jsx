65cedbe13e5ab5881c24d0f01cfcbaf8c0d16577
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2024-10-10 05:13:25 +0530

    Refactor ActivityBar component transitions and styles

--- src/components/activity-bar/ActivityBar.jsx
+++ src/components/activity-bar/ActivityBar.jsx
@@ -14,42 +14,42 @@ function ActivityBar() {
   },[isHomePage]);
   
   const renderCollapseButton = (expanded) => {
-    if (expanded) return (
+    const style = expanded 
+      ? 
+        'flex items-center gap-2 py-2 px-2.5 rounded-lg'
+      :
+        'w-fit p-1.5 ml-1 rounded-full';
+
+    return (
       <button 
-        className='flex items-center gap-2 py-2 px-2.5 rounded-lg hover:bg-[#1b234d]'
-        onClick={() => setExpanded(false)}
+        className={style + ' activity-bar-item hover:bg-[#1b234d]'}
+        onClick={() => setExpanded(!expanded)}
       >
-        <activityBarIcons.ChevronsRight className='w-8 h-8 -rotate-180' stroke='#7F7D76' />
-        <p className='act-bar-label text-lg overflow-hidden w-fit' style={{color: '#7F7D76'}}>Collapse</p>
-      </button>
-    )
-    else return (
-      <button 
-        className='w-fit p-1.5 ml-1 rounded-full hover:bg-[#1b234d]'
-        onClick={() => setExpanded(true)}
-      >
-        <activityBarIcons.ChevronsRight className='w-8 h-8' stroke='#7F7D76' />
+        <activityBarIcons.ChevronsLeft className={`w-8 h-8 chevron-left ${expanded ? 'rotate-0': '-rotate-180'}`} stroke='#7F7D76' />
+        { expanded && <p className='act-bar-label text-lg overflow-hidden w-fit' style={{color: '#7F7D76'}}>Collapse</p> }
       </button>
     );
   }
 
   return (
     <nav 
-      className={`activity-bar bg-[#111] h-[100vh] flex flex-col px-1.5 justify-evenly ${expanded ? 'w-[14rem]': 'w-20'}`}
+      className={`activity-bar bg-[#111] h-[100vh] flex flex-col px-1.5 pt-6 pb-3 ${expanded ? 'w-[13rem]': 'w-20'}`}
     >
       <Link to="/" className='flex ml-1.5 items-end'>
         <activityBarIcons.Logo className='w-12 h-12 fixed' />
         <p className={`act-bar-label text-white text-3xl ml-[2.75rem] overflow-hidden ${expanded ? 'w-fit': 'w-0'}`}>TaskOS</p>
       </Link>
 
-      <ul className='flex flex-col mt-[3.5rem] gap-3'>
-        <ActivityBarItem Icon={activityBarIcons.Sun} label="Today" to="/today" expanded={expanded} />
+      <ul className='flex flex-col gap-2 mt-[4.75rem] flex-grow'>
+        <ActivityBarItem Icon={activityBarIcons.Home} label="Home" to="/" expanded={expanded} />
+        <ActivityBarItem Icon={activityBarIcons.Timeline} label="Timeline" to="/timeline" expanded={expanded} />
         <ActivityBarItem Icon={activityBarIcons.Rocket} label="Projects" to="/dashboard" expanded={expanded} />
         <ActivityBarItem Icon={activityBarIcons.Star} label="Starred" to="/starred" expanded={expanded} />
+        <ActivityBarItem Icon={activityBarIcons.MyTasks} label="My Tasks" to="/my-tasks" expanded={expanded} />
         <ActivityBarItem Icon={activityBarIcons.Notes} label="Notes" to="/notes" expanded={expanded} />        
       </ul>
 
-      <ul className='flex flex-col mt-[3.5rem] gap-0.5'>
+      <ul className='flex flex-col gap-0.5'>
         { renderCollapseButton(expanded) }
         <ActivityBarItem Icon={activityBarIcons.Settings} label="Settings" to="/settings" expanded={expanded} stroke="#7F7D76" />
         <ActivityBarItem Icon={activityBarIcons.QuestionMark} label="Help" to="/help" expanded={expanded} stroke="#7F7D76" />
--- src/components/activity-bar/ActivityBarItem.jsx
+++ src/components/activity-bar/ActivityBarItem.jsx
@@ -2,15 +2,14 @@ import { Link } from "react-router-dom"
 import '../../styles/animations.css'
 
 function ActivityBarItem({ Icon, label, to, expanded, stroke='#fff' }) {
-    if (expanded) return (
-        <Link to={to} className="activity-bar-item flex items-center py-2 px-3 rounded-lg hover:bg-[#1b234d]">
-            <Icon className='w-7 h-7 fixed' stroke={stroke} />
-            <p className={`act-bar-label text-lg ml-10 overflow-hidden`} style={{color: stroke}}>{label}</p>
-        </Link>
-    )
-    else return (
-        <Link to={to} className="activity-bar-item ml-1.5 p-2 rounded-full w-fit hover:bg-[#1b234d]">
-            <Icon className='w-7 h-7' stroke={stroke} />
+    const style = expanded ?
+        'flex items-center py-2 px-3 rounded-lg overflow-hidden' :
+        `ml-1.5 p-2 rounded-full w-fit h-fit`;
+
+    return (
+        <Link to={to} className={style + ' activity-bar-item hover:bg-[#1b234d]'}>
+            <Icon className={`w-7 h-7 ${expanded ? 'fixed':''} `} stroke={stroke} />
+            <p className={`text-lg ml-10 text-nowrap ${expanded ? 'relative opacity-100': 'absolute opacity-0'} `} style={{color: stroke}}>{label}</p>
         </Link>
     );
 }
