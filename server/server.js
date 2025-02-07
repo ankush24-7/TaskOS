3e906d6073be4ae2934806845e924928a9df22c1
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-02-08 05:10:44 +0530

    feat: add database connection

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const logger = require('./utils/logger');
+const connectDB = require('./config/dbConn');
+const cookieParser = require('cookie-parser');
+
+connectDB();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/refresh', require('./routes/refreshRoutes'));
+app.use('/logout', require('./routes/logoutRoutes'));
+
+// app.use('/user', require('./routers/userRouter'));
+// app.use('/project', require('./routers/projectRouter'));
+// app.use('/dashboard', require('./routers/dashboardRouter'));
+// app.use('/section', require('./routers/sectionRouter'));
+// app.use('/process', require('./routers/processRouter'));
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+})
