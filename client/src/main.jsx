9a8b5a42708988fca097776a8587bd966c0ec746
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-02-12 17:06:51 +0530

    feat: restructure routing in App component to use Outlet for nested routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import './index.css';
 import App from './App.jsx'
-import './index.css'
+import "@styles/media-query.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import HomePage from "@pages/home-page/HomePage";
+import LoginPage from "@pages/auth-page/LoginPage";
+import NotesPage from "@pages/notes-page/NotesPage";
+import GuestPage from "@pages/guest-page/GuestPage";
+import SignUpPage from "@pages/auth-page/SignUpPage";
+import Projects from "@pages/projects-page/ProjectsPage";
+import NoteEditorPage from "@pages/notes-page/NoteEditorPage";
+import DashBoardPage from "@pages/dashboard-page/DashboardPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="/" element={<GuestPage />} exact />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/home" element={<HomePage />} exact />
+          <Route path="/projects" element={<Projects />} exact />
+          <Route path="/projects/:projectId/dashboard" element={<DashBoardPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/notes/:notesTitle/:id" element={<NoteEditorPage />} />
+        </Route>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
