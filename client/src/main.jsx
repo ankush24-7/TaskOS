f4d35cd8ba773b28f462f1feb294ab94069b1fa7
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-03-12 20:14:48 +0530

    feat: remove notes-related components and data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
 import './index.css';
-import '@styles/global.css';
 import App from './App.jsx';
+import '@styles/global.css';
 import "@styles/scrollbars.css";
+import "@styles/text-editor.css";
 import "@styles/media-query.css";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import HomePage from "@pages/home-page/HomePage";
 import LoginPage from "@pages/auth-page/LoginPage";
-import NotesPage from "@pages/notes-page/NotesPage";
 import GuestPage from "@pages/guest-page/GuestPage";
 import SignUpPage from "@pages/auth-page/SignUpPage";
 import Projects from "@pages/projects-page/ProjectsPage";
-import NoteEditorPage from "@pages/notes-page/NoteEditorPage";
 import DashBoardPage from "@pages/dashboard-page/DashboardPage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -26,8 +25,6 @@ createRoot(document.getElementById('root')).render(
           <Route path="/home" element={<HomePage />} exact />
           <Route path="/projects" element={<Projects />} exact />
           <Route path="/projects/:projectId/dashboard" element={<DashBoardPage />} />
-          <Route path="/notes" element={<NotesPage />} />
-          <Route path="/notes/:notesTitle/:id" element={<NoteEditorPage />} />
         </Route>
       </Routes>
     </Router>
