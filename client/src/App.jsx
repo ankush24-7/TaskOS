a8c6e5e131930c57c5c2c503d11e53d3a67cf71e
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-03-12 20:25:00 +0530

    feat: remove AuthProvider and simplify App component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
 import ActivityBar from "@components/activity-bar/ActivityBar";
 
 function App() {
   return (
     <div className="h-dvh flex flex-col-reverse antialiased sm:flex-row">
-      <AuthProvider>
-        <ActivityBar />
-        <Outlet />
-      </AuthProvider>
+      <ActivityBar />
+      <Outlet />
     </div>
   );
 }
