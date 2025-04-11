cdc05c7b9cbe6dbf8a29384d29d0dc2ed4cbd79a
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-04-11 19:11:57 +0530

    feat: add Settings and TimelinePage routes; update package dependencies for cloudinary, multer, and streamifier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,8 +11,10 @@ import HomePage from "@pages/home-page/HomePage";
 import LoginPage from "@pages/auth-page/LoginPage";
 import GuestPage from "@pages/guest-page/GuestPage";
 import SignUpPage from "@pages/auth-page/SignUpPage";
-import Projects from "@pages/projects-page/ProjectsPage";
+import Settings from './pages/settings-page/Settings';
 import { ToastProvider } from './contexts/ToastContext';
+import Projects from "@pages/projects-page/ProjectsPage";
+import TimelinePage from './pages/timeline-page/TimelinePage';
 import DashBoardPage from "@pages/dashboard-page/DashboardPage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -26,7 +28,9 @@ createRoot(document.getElementById('root')).render(
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/" element={<App />} >
             <Route path="/home" element={<HomePage />} exact />
+            <Route path="/timeline" element={<TimelinePage />} />
             <Route path="/projects" element={<Projects />} exact />
+            <Route path="/settings" element={<Settings />} />
             <Route path="/projects/:projectId/dashboard" element={<DashBoardPage />} />
           </Route>
         </Routes>
