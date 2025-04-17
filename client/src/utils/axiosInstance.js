945db54ec2d33460f600459173e047650598e192
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-04-18 00:50:11 +0530

    fix: correct favicon path in index.html and update axios baseURL to a static value

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authAPI from "@/services/authAPI";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://taskos.onrender.com",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
