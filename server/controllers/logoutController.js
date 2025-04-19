140d8b0ca9de153318e9f14d59c06ae11ca4ea62
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-04-19 19:45:52 +0530

    fix: enhance cookie clearing in handleLogout to include sameSite and secure attributes

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -7,13 +7,21 @@ const handleLogout = async (req, res) => {
 
   const user = await User.findOne({ refreshToken }).exec();
   if (!user) {
-    res.clearCookie("jwt", { httpOnly: true });
+    res.clearCookie("jwt", { 
+      httpOnly: true,
+      sameSite: "None",
+      secure: process.env.NODE_ENV === "production",
+    });
     return res.sendStatus(204);
   }
 
   user.refreshToken = "";
   const response = await user.save();
-  res.clearCookie("jwt", { httpOnly: true });
+  res.clearCookie("jwt", { 
+    httpOnly: true,
+    sameSite: "None",
+    secure: process.env.NODE_ENV === "production",
+  });
   res.status(204).json({ status: "Success", response });
 };
 
