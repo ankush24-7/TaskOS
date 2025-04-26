edc3307184a7545b54e18939af56aa7f282f667e
ankush24-7 <ankush_it22a8_56@dtu.ac.in>
2025-04-26 19:09:12 +0530

    feat: add Lottie animations and logo to GuestPage for enhanced loading experience

--- client/src/components/ui/Logo.jsx
+++ client/src/components/ui/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <Link to="/" className="flex items-center">
       <LogoIcon className="w-12 h-12 -translate-y-1.5 translate-x-1.5" />
-      <p className="act-bar-label text-white text-4xl font-inconsolata">
+      <p className="act-bar-label text-4xl font-inconsolata text-white">
         TaskOS
       </p>
     </Link>
--- client/src/pages/guest-page/GuestPage.jsx
+++ client/src/pages/guest-page/GuestPage.jsx
@@ -1,9 +1,10 @@
+import Logo from "@/components/ui/Logo";
 import authAPI from "@/services/authAPI";
 import { useEffect, useState } from "react";
 import GuestNav from "./components/GuestNav";
 import { useNavigate } from "react-router-dom";
 import GuestHeroSection from "./components/GuestHeroSection";
-import SpinLoader from "@/components/loaders/SpinLoader";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const GuestPage = () => {
   const navigate = useNavigate();
@@ -16,7 +17,7 @@ const GuestPage = () => {
         navigate("/home", { replace: true });
       }
       setIsLoading(false);
-    }
+    };
 
     checkToken();
   }, []);
@@ -24,8 +25,20 @@ const GuestPage = () => {
   return (
     <div className="h-dvh w-full bg-gradient-to-r from-grad-l to-grad-r">
       {isLoading ? (
-        <div className="w-full h-full flex justify-center items-center pb-6">
-          <SpinLoader width="40px" height="40px" />
+        <div className="w-full h-full flex flex-col items-center pt-20">
+          <span className="absolute top-5 left-10">
+            <Logo />
+          </span>
+          <DotLottieReact
+            className="w-1/2 h-1/2"
+            src="https://lottie.host/32b41da0-a43e-49f0-b6f8-136e591cc97a/LfYxELDKLA.lottie"
+            loop
+            autoplay
+            backgroundColor="transparent"
+            speed={1.25}
+          />
+          <h2 className="text-3xl mt-5 mb-2 text-white">Waking up the server...</h2>
+          <p className="text-lg text-center max-w-140 leading-6 text-white">Thank you for your patience. This usually takes a minute or two!</p>
         </div>
       ) : (
         <div className="w-full h-full relative flex flex-col">
